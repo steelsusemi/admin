465ac1b94e4f7692e8347a8a5e34d043d2afc655
steelsusemi <whwncjs95@gmail.com>
2021-10-05 18:08:29 +0900

    properties active 별루 처리

--- src/main/java/com/jjplatform/admin/util/PropertisConfig.java
+++ src/main/java/com/jjplatform/admin/util/PropertisConfig.java
@@ -2,10 +2,12 @@ package com.jjplatform.admin.util;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.util.StringUtils;
@@ -13,19 +15,17 @@ import org.springframework.util.StringUtils;
 @Configuration
 public class PropertisConfig {
 	private static Logger log = LoggerFactory.getLogger(PropertisConfig.class);
-	
-	@Value("${spring.profiles.active}")
-	private String activeProfiles;
-	
+
 	@Bean
 	public PropertySourcesPlaceholderConfigurer propertisFactoryBean() {
 		log.info("####################[PropertisConfig]####################");
+		String activeProfiles = System.getProperty("spring.profiles.active");
+		log.info("## activeProfiles :: " + activeProfiles);
 		
 		PropertySourcesPlaceholderConfigurer propertiesBean = new PropertySourcesPlaceholderConfigurer();
 		String profiles = StringUtils.isEmpty(activeProfiles) ?  "local" : activeProfiles; 
 		
-		log.info("## activeProfiles :: "+activeProfiles);
-		log.info("## profiles :: "+profiles);
+		log.info("## profiles :: " + profiles);
 		
 //		System.setProperty("spring.profiles.active", profiles);
 		Resource[] resources = new ClassPathResource[]{
